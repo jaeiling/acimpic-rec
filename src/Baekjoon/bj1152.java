15555fdf7fb384a12c0fb0ceb936cd1d434ae91d
Jaeil An <a090066@naver.com>
2025-04-07 01:26:54 +0900

    error fix

--- src/Baekjoon/bj1152.java
+++ src/Baekjoon/bj1152.java
@@ -1,10 +1,12 @@
+package Baekjoon;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
 
 public class bj1152 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
 
